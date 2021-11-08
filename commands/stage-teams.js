6e2c07c24219bc07113d4a64814106e3bc609dc4
Darshan Patel <heyforscht@gmail.com>
2021-11-09 03:10:00 +0530

    [MOD] Fix bug

--- commands/stage-teams.js
+++ commands/stage-teams.js
@@ -1,17 +1,11 @@
 const valorantAPI = require('../models/valorantAPI')
-const Components = require('../struct/components')
 
 module.exports = {
     name: 'stage-teams',
+    adminOnly: true,
     async exec(interaction) {
-        const { client, user } = interaction
+        const { client } = interaction
         await interaction.deferReply()
-        // checking if the user is admin or not.
-        if (process.env.ADMINS.split(',').includes(user.id)) {
-            const embed = Components.errorEmbed('You are not an admin in this server to use this command')
-
-            return interaction.editReply({ embeds: [embed] })
-        }
 
         const teams = await client.factory.getRegisteredTeams()
         const teamsToStage = teams.filter((team) => team.players.length >= 5 && team.rating === 0)
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,10 +1,17 @@
 
+const admins = process.env.ADMINS.split(',')
+
 module.exports = async (client, interaction) => {
     if (!interaction.isCommand()) return
     const command = client.slashCommands.get(interaction.commandName)
     if (command) {
         try {
-            await command.exec(interaction)
+            if (command.adminOnly && !admins.includes(interaction.user.id)) {
+                // TODO : Put embed here
+                await interaction.reply(`You're not allowed to use this command`)
+            } else {
+                await command.exec(interaction)
+            }
         } catch (err) {
             client.util.errorPrint(err, { description: `command error :: ${interaction.commandName} | ${interaction.guild.name} | ${interaction.channel.name} | ${interaction.user.tag}` })
         }
