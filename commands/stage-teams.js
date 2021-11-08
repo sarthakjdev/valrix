8551e2c6b3c88a3851dea4f3da3bae437ce78c13
sarthakjdev <jsarthak448@gmail.com>
2021-11-08 13:49:00 +0530

    stage team command adde and its working

--- commands/stage-teams.js
+++ commands/stage-teams.js
@@ -16,37 +16,32 @@ module.exports = {
     // team staging process here
     async stageTeams(interaction, teams) {
         const { client } = interaction
-        // const teams = await client.factory.getRegisteredTeams()
         const teamPlayers = await Promise.all(teams.map((team) => client.factory.getTeamPlayers(team)))
-        await teamPlayers.forEach((playersOfTeam) => {
-            if (playersOfTeam.length === 4) {
-                // this promise is not getting resolved
-                const playerRatings = Promise.all(playersOfTeam.map((p) => valorantAPI.getPlayerRating(process.env.REGION, p.valorantName, p.valorantTag)))
-                console.log('playerRatings ', playerRatings)
-
-                // Calculating team rating to rank on leaderboard
-                playerRatings.forEach((teamPlayersRatings) => {
+        await teamPlayers.map(async (playersOfTeam) => {
+            if (playersOfTeam.length === 5) {
+                const playersRatings = await Promise.all(playersOfTeam.map((p) => valorantAPI.getPlayerRating(process.env.REGION, p.valorantName, p.valorantTag)))
+                playersRatings.map((teamPlayersRatings) => {
                     const avg = teamPlayersRatings.reduce((sum, player) => player.elo + sum, 0) / teamPlayersRatings.length
                     const aggregateTeamRating = 1700 - (400 * (1 - (avg / 105)))
-                    console.log(playerRatings)
+
                     client.factory.updateTeamRating(playersOfTeam[0].team, aggregateTeamRating)
-                    interaction.editReply(':white_check_mark: Team has been staged to rank')
+
+                    return interaction.editReply(':white_check_mark: Team has been staged to rank')
                 })
             }
+            const embed = Components.errorEmbed('There\'s no eligible team for staging')
+
+            return interaction.editReply({ embeds: [embed] })
         })
-        console.log('reached end of file')
     },
     async exec(interaction) {
         const { client, user } = interaction
         await interaction.deferReply()
         const ownerInput = interaction.options.get('owner')?.value
         const owner = ownerInput || user.id
-        console.log(`user input or not ${owner}`)
         const userPlayer = await client.factory.getPlayerById(owner)
-
         // checking if the user is admin or not.
-        if (process.env.ADMINS.split(',').includes(user.id)) return this.checkTeam(interaction, userPlayer)
-
+        if (process.env.ADMINS.split(',').includes(user.id)) return this.setTeams(interaction, userPlayer)
         const embed = Components.errorEmbed('You are not an admin in this server to use this command')
 
         return interaction.editReply({ embeds: [embed] })
