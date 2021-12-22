b9db2737a6280229ddeaf3320309eafe81f2306c
sarthakjdev <jsarthak448@gmail.com>
2021-12-22 09:52:52 +0530

    [MOD] Added condition in leaderboard if no team in db

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -12,7 +12,7 @@ module.exports = {
         await interaction.deferReply()
         // extracting teams in desc order of their rating:
         const teams = await client.factory.getLeaderboard()
-        if (!teams) {
+        if (teams.length === 0) {
             const errorEmbed = await Components.errorEmbed('No team to be ranked in the leaderboard')
 
             return interaction.editReply(errorEmbed)
